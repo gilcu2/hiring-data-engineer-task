b447044692c494937340642c058eb71de2b91fb7
Reynaldo Gil <gilcu2@gmail.com>
2025-04-04 16:08:35 +0200

    working on clickhouse migrations

---
+++ migrations/clickhouse/V1__create_schema.sql
@@ -0,0 +1,47 @@
+CREATE TABLE clicks
+(
+    id UInt64,
+    advertiser_id UInt32,
+    campaign_id UInt32,
+    timestamp DateTime,
+)
+ENGINE = MergeTree()
+PARTITION BY toYYYYMMDD(timestamp)
+ORDER BY (timestamp, advertiser_id, campaign_id, id)
+
+CREATE TABLE impressions
+(
+    id UInt64,
+    advertiser_id UInt32,
+    campaign_id UInt32,
+    timestamp DateTime,
+)
+ENGINE = MergeTree()
+PARTITION BY toYYYYMMDD(timestamp)
+ORDER BY (timestamp, advertiser_id, campaign_id, id)
+
+CREATE TABLE advertiser
+(
+    id UInt32,
+    name String,
+    updated_at DateTime,
+    created_at DateTime
+)
+ENGINE = MergeTree()
+ORDER BY id
+
+CREATE TABLE campaign
+(
+    id UInt32,
+    name String,
+    bid DECIMAL(10,2),
+    budget DECIMAL(10,2),
+    start_date DATE32,
+    end_date DATE32,
+    advertiser_id UInt32,
+    updated_at DateTime,
+    created_at DateTime
+)
+ENGINE = MergeTree()
+ORDER BY campaign_id
+
